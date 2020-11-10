ec2a5092ec508dda02242414d5065ca732dfdc61
taolx <taolx25@foxmail.com>
2020-11-10 09:14:01 +0800

    fix waring

--- sk-app/config/config.go
+++ sk-app/config/config.go
@@ -2,12 +2,9 @@ package config
 
 import (
 	"github.com/go-kit/kit/log"
-	"github.com/openzipkin/zipkin-go"
-	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
 	_ "github.com/openzipkin/zipkin-go/reporter/recorder"
 	"github.com/spf13/viper"
 	"os"
-	"secKill/pkg/bootstrap"
 	_ "secKill/pkg/bootstrap"
 	conf "secKill/pkg/config"
 	"secKill/sk-app/model"
@@ -18,7 +15,7 @@ const (
 	kConfigType = "CONFIG_TYPE"
 )
 
-var ZipkinTracer *zipkin.Tracer
+//var ZipkinTracer *zipkin.Tracer
 var Logger log.Logger
 
 func init() {
@@ -29,30 +26,27 @@ func init() {
 	initDefault()
 
 	if err := conf.LoadRemoteConfig(); err != nil {
-		Logger.Log("Fail to load remote config", err)
+		_ = Logger.Log("Fail to load remote config", err)
 	}
 
 	if err := conf.Sub("mysql", &conf.MysqlConfig); err != nil {
-		Logger.Log("Fail to parse mysql", err)
+		_ = Logger.Log("Fail to parse mysql", err)
 	}
-	//if err := conf.Sub("trace", &conf.TraceConfig); err != nil {
-	//	Logger.Log("Fail to parse trace", err)
-	//}
 
 	//if err := conf.Sub("trace", &conf.TraceConfig); err != nil {
-	//	Logger.Log("Fail to parse trace", err)
+	//	_ = Logger.Log("Fail to parse trace", err)
 	//}
 
 	if err := conf.Sub("service", &conf.SecKill); err != nil {
-		Logger.Log("Fail to parse trace", err)
+		_ = Logger.Log("Fail to parse trace", err)
 	}
 
 	if err := conf.Sub("redis", &conf.Redis); err != nil {
-		Logger.Log("Fail to parse trace", err)
+		_ = Logger.Log("Fail to parse trace", err)
 	}
 
 	//zipkinUrl := "http://" + conf.TraceConfig.Host + ":" + conf.TraceConfig.Port + conf.TraceConfig.Url
-	//Logger.Log("zipkin url", zipkinUrl)
+	//_ = Logger.Log("zipkin url", zipkinUrl)
 	//initTracer(zipkinUrl)
 }
 
@@ -60,25 +54,25 @@ func initDefault() {
 	viper.SetDefault(kConfigType, "yaml")
 }
 
-func initTracer(zipkinURL string) {
-	var (
-		err           error
-		useNoopTracer = zipkinURL == ""
-		reporter      = zipkinhttp.NewReporter(zipkinURL)
-	)
-	//defer reporter.Close()
-	zEP, _ := zipkin.NewEndpoint(bootstrap.DiscoverConfig.ServiceName, bootstrap.HttpConfig.Port)
-	ZipkinTracer, err = zipkin.NewTracer(
-		reporter, zipkin.WithLocalEndpoint(zEP), zipkin.WithNoopTracer(useNoopTracer),
-	)
-	if err != nil {
-		Logger.Log("err", err)
-		os.Exit(1)
-	}
-	if !useNoopTracer {
-		Logger.Log("tracer", "Zipkin", "type", "Native", "URL", zipkinURL)
-	}
-}
+//func initTracer(zipkinURL string) {
+//	var (
+//		err           error
+//		useNoopTracer = zipkinURL == ""
+//		reporter      = zipKinHttp.NewReporter(zipkinURL)
+//	)
+//	//defer reporter.Close()
+//	zEP, _ := zipkin.NewEndpoint(bootstrap.DiscoverConfig.ServiceName, bootstrap.HttpConfig.Port)
+//	ZipkinTracer, err = zipkin.NewTracer(
+//		reporter, zipkin.WithLocalEndpoint(zEP), zipkin.WithNoopTracer(useNoopTracer),
+//	)
+//	if err != nil {
+//		_ = Logger.Log("err", err)
+//		os.Exit(1)
+//	}
+//	if !useNoopTracer {
+//		_ = Logger.Log("tracer", "Zipkin", "type", "Native", "URL", zipkinURL)
+//	}
+//}
 
 var SkAppContext = &SkAppCtx{
 	UserConnMap: make(map[string]chan *model.SecResult, 1024),
@@ -95,7 +89,7 @@ type SkAppCtx struct {
 }
 
 const (
-	ProductStatusNormal       = 0 //商品状态正常
+	//ProductStatusNormal       = 0 //商品状态正常
 	ProductStatusSaleOut      = 1 //商品售罄
 	ProductStatusForceSaleOut = 2 //商品强制售罄
 )
