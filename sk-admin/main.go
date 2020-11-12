e5bc00430d6c0df12559f1f11c8271c78f57f1f0
taolx0 <taolx25@foxmail.com>
2020-11-12 22:10:05 +0800

    change zookeeper to etcd

--- sk-admin/main.go
+++ sk-admin/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	mysql.InitMysql(conf.MysqlConfig.Host, conf.MysqlConfig.Port, conf.MysqlConfig.User, conf.MysqlConfig.Pwd, conf.MysqlConfig.Db) // conf.MysqlConfig.Db
-	//setup.InitEtcd()
-	setup.InitZk()
+	setup.InitEtcd()
+	//setup.InitZk()
 	setup.InitServer(bootstrap.HttpConfig.Host, bootstrap.HttpConfig.Port)
 }
