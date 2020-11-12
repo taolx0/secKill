432862c0a56d5ef6519ffdab77c75c71474716c1
taolx <taolx25@foxmail.com>
2020-11-12 13:28:30 +0800

    fix warning

--- sk-app/service/srv_err/error.go
+++ sk-app/service/srv_err/error.go
@@ -5,31 +5,31 @@ import (
 )
 
 const (
-	ErrInvalidRequest      = 1101
-	ErrNotFoundProductId   = 1102
-	ErrUserCheckAuthFailed = 1103
-	ErrUserServiceBusy     = 1104
-	ErrActiveNotStart      = 1105
-	ErrActiveAlreadyEnd    = 1106
-	ErrActiveSaleOut       = 1107
-	ErrProcessTimeout      = 1108
-	ErrClientClosed        = 1109
+	//ErrInvalidRequest      = 1101
+	ErrNotFoundProductId = 1102
+	//ErrUserCheckAuthFailed = 1103
+	ErrUserServiceBusy  = 1104
+	ErrActiveNotStart   = 1105
+	ErrActiveAlreadyEnd = 1106
+	ErrActiveSaleOut    = 1107
+	ErrProcessTimeout   = 1108
+	ErrClientClosed     = 1109
 )
 
 const (
 	ErrServiceBusy     = 1001
-	ErrSecKillSucc     = 1002
+	ErrSecKillSuccess  = 1002
 	ErrNotFoundProduct = 1003
-	ErrSoldout         = 1004
+	ErrSoldOut         = 1004
 	ErrRetry           = 1005
 	ErrAlreadyBuy      = 1006
 )
 
 var errMsg = map[int]string{
 	ErrServiceBusy:     "服务器错误",
-	ErrSecKillSucc:     "抢购成功",
+	ErrSecKillSuccess:  "抢购成功",
 	ErrNotFoundProduct: "没有该商品",
-	ErrSoldout:         "商品售罄",
+	ErrSoldOut:         "商品售罄",
 	ErrRetry:           "请重试",
 	ErrAlreadyBuy:      "已经抢购",
 }
