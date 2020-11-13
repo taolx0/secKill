18ed54f307949063cab29ba8fc9a90e190180b7c
taolx0 <taolx25@foxmail.com>
2020-11-13 15:27:50 +0800

    add tips

--- sk-admin/service/activity.go
+++ sk-admin/service/activity.go
@@ -65,8 +65,8 @@ func (p ActivityServiceImpl) CreateActivity(activity *model.Activity) error {
 		log.Printf("ActivityModel.CreateActivity, err : %v", err)
 		return err
 	}
-
-	log.Printf("syncToEtcd")
+	log.Println("write to database success!")
+	log.Println("syncToEtcd")
 	//写入到Etcd
 	err = p.syncToEtcd(activity)
 	if err != nil {
