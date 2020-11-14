2a133000525874d837317621c0aad51a3df32d2a
taolx0 <taolx25@foxmail.com>
2020-11-14 08:31:15 +0800

    InitEtcd

--- sk-app/main.go
+++ sk-app/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	mysql.InitMysql(conf.MysqlConfig.Host, conf.MysqlConfig.Port, conf.MysqlConfig.User, conf.MysqlConfig.Pwd, conf.MysqlConfig.Db) // conf.MysqlConfig.Db
-	setup.InitZk()
+	//setup.InitZk()
+	setup.InitEtcd()
 	setup.InitRedis()
 	setup.InitServer(bootstrap.HttpConfig.Host, bootstrap.HttpConfig.Port)
 }
